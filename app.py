970ea1cded0493b8ba92f8d628c26377aeb6b7e7
Fernando Marins <f.augustomarins@gmail.com>
2023-12-20 13:47:58 -0300

    Adding html templates

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-"""An AWS Python Pulumi program"""
+import os
+from flask import Flask, render_template
 
-import pulumi
-from pulumi_aws import s3
+import pulumi.automation as auto
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+def ensure_plugins():
+    ws = auto.LocalWorkspace()
+    ws.install_plugin("aws", "v4.0.0")
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+def create_app():
+    ensure_plugins()
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="secret",
+        PROJECT_NAME="herocool",
+        PULUMI_ORG=os.environ.get("PULUMI_ORG"),
+    )
+
+    @app.route("/", methods=["GET"])
+    def index():
+        return render_template("index.html")
+
+    import sites
+
+    app.register_blueprint(sites.bp)
+
+    import virtual_machines
+
+    app.register_blueprint(virtual_machines.bp)
+
+    return app
